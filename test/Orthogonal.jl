d638588f7b3ef78403d53edd43a62b5067ff2e37
jverzani <jverzani@gmail.com>
2021-07-10 08:52:03 -0400

    adjust test

--- test/Orthogonal.jl
+++ test/Orthogonal.jl
@@ -374,7 +374,7 @@ end
     for P in union(Ps, DPs)
         P <: Bessel && continue
         for n in 6:2:10
-            rts = roots(basis(P, n))
+            rts = roots(convert(Polynomial, basis(P, n)))
             evals = eigvals(SP.jacobi_matrix(P, n))
             @test maximum(abs,  rts - evals) <= 1e-4
         end
