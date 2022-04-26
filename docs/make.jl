ba6fb8269b29a3b274f5cef3a7b12fb6a350f403
jverzani <jverzani@gmail.com>
2022-04-25 21:27:22 -0400

    use gh-pages

--- docs/make.jl
+++ docs/make.jl
@@ -10,10 +10,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-    )=#
-
 deploydocs(
     repo = "github.com/jverzani/SpecialPolynomials.jl.git",
 )
