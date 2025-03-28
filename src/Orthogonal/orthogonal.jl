cac116376442cd3ef93032a8af998646c80cd527
jverzani <jverzani@gmail.com>
2025-03-28 11:08:37 -0400

    more Basis comment

--- src/Orthogonal/orthogonal.jl
+++ src/Orthogonal/orthogonal.jl
@@ -197,7 +197,7 @@ end
      Basis(P,n)
      Basis{P}(n)
 
-The  command `basis(P,n, [var])` realizes the polynomial. `Basis(P,n)` does  not.  This  can  be  useful for  evaluation.
+The  command `basis(P,n, [var])` realizes the polynomial. `Basis(P,n)` does  not.  This  can  be  useful for  evaluation but *only* for small values of ``n``, as the evaluation via a hypergeometric representation is susceptible to numeric issues.
 """
 struct Basis{Π}
     n::Int
